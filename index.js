f7ab33ca22af7ea115cee9221ebce04b7a57c1ab
joyce wamocho <joywamo@gmail.com>
2024-09-28 22:48:57 +0300

    looping code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, surprise = "surprise") {
+    const card = [];
+    for (let count = 0; count < names.length; count++) {
+        card.push(`Thank you, ${names[count]}, for the wonderful ${surprise} gift!`);
+    }
+    return card;
+}
+
+
+function countDown(){
+    let count=10;
+    while(count >= 0){
+        console.log (count--);
+    }
+}
+countDown();
+
